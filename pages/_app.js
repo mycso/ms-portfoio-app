d3a728c6fee0b8db78ee9b70d5dba0f0083aa94a
Michael Sokan <michael.sokan@dotcentric.co.uk>
2022-07-26 11:18:09 +0100

    amends to backend

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ import { ApolloProvider } from '@apollo/client';
 import withData from '../lib/withData';
 import { CartStateProvider } from '../lib/cartState';
 import { CheckoutStateProvider } from '../lib/checkoutState';
+import CMS from 'netlify-cms-app'
+// Initialize the CMS object
+CMS.init()
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate)
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 
