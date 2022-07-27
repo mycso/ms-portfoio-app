6884fe0f62b3f4b90b043dc4a1347cc4c46a1053
Michael Sokan <michael.sokan@dotcentric.co.uk>
2022-07-27 16:34:21 +0100

    amends to the node-modules

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,7 +1,7 @@
 
 import React from "react"
 import styled from 'styled-components';
-import { Redirect } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 import Markdown from "react-markdown"
 import postlist from "../posts.json"
 import SingleItemStyles from '../../src/components/styles/SingleItemStyles';
@@ -37,7 +37,7 @@ const Post = (props) => {
         }
     })
     if (postExists === false) {
-        return <Redirect to="/404" />
+        return <Navigate to="/404" />
     }
 
     const dataImage = [
